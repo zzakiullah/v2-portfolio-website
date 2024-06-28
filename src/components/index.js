8866d539d7b9c4f8274f5752a7fcf0997ecb1e03
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2024-06-28 17:38:22 -0400

    Create nextjs project

--- src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Header } from './Header';
-export { default as Footer } from './Footer';
-export { default as Modal } from './Modal';
-export { default as Settings } from './Settings';
-export { default as Toggler } from './Toggler';
--- src/sections/index.js
+++
@@ -1,4 +0,0 @@
-export { default as About } from './About';
-export { default as Experience } from './Experience';
-export { default as Projects } from './Projects';
-export { default as Contact } from './Contact';
