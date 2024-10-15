cab20aa88f0abc57810084e369d9d5ee784d561f
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2024-10-14 20:42:05 -0400

    Add to nav

--- src/components/MyNav.tsx
+++ src/components/MyNav.tsx
@@ -4,6 +4,18 @@ import { useEffect, useState } from "react";
 import NextLink from "next/link";
 import { Box, Flex, Link, Image, List, ListItem } from "@chakra-ui/react";
 import { Twirl as Hamburger } from "hamburger-react";
+import {
+    MdHome,
+    MdFace,
+    MdBusinessCenter,
+    MdFolderOpen,
+    MdChat,
+    MdOutlineHome,
+    MdOutlineFace,
+    MdOutlineBusinessCenter,
+    MdOutlineFolderOpen,
+    MdOutlineChat,
+} from "react-icons/md";
 
 import useScrollDirection from "@/hooks/useScrollDirection";
 
@@ -17,7 +29,48 @@ export default function MyNav() {
             as={"nav"}
             position={"sticky"}
         >
-            
+            <List>
+                <ListItem>
+                    <Link
+                        as={NextLink}
+                        href={"/"}
+                    >
+                        Home
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Link
+                        as={NextLink}
+                        href={"/about"}
+                    >
+                        About
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Link
+                        as={NextLink}
+                        href={"/experience"}
+                    >
+                        Experience
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Link
+                        as={NextLink}
+                        href={"/projects"}
+                    >
+                        Projects
+                    </Link>
+                </ListItem>
+                <ListItem>
+                    <Link
+                        as={NextLink}
+                        href={"/contact"}
+                    >
+                        Contact
+                    </Link>
+                </ListItem>
+            </List>
         </Flex>
     );
 }
