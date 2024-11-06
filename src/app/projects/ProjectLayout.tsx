7d1c402d047b6f4cd0022d7f4145b4ae5bb1903b
Zulaikha Zakiullah <zzakiullah1@gmail.com>
2024-11-06 15:45:02 -0500

    Start project layout with filters

--- src/app/projects/ProjectLayout.tsx
+++ src/app/projects/ProjectLayout.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { useState } from "react";
-import { Flex, Heading, Text, Link, Tag, Wrap, WrapItem } from "@chakra-ui/react";
-// import { GoArrowUpRight } from "react-icons/go";
+import { useEffect, useState } from "react";
+import { Flex, Box, Heading, Text, Input, Select } from "@chakra-ui/react";
+import { MdCategory, MdSwapVert, MdFormatListBulleted, MdApps } from "react-icons/md";
 
 import { figtree } from "@/constants/fonts";
 import ProjectItem from "./ProjectItem";
@@ -19,20 +19,38 @@ interface IProject {
 export default function ProjectLayout() {
     const [filteredProjects, setFilteredProjects] = useState<IProject[]>([]);
 
+    useEffect(() => {
+
+    }, []);
+
     return (
-        <Flex>
-            {projectData.map((data, index) => {
-                return (
-                    <ProjectItem
-                        key={index}
-                        title={data.title}
-                        description={data.description}
-                        srcUrl={data.srcUrl}
-                        demoUrl={data.demoUrl}
-                        tags={data.tags}
-                    />
-                );
-            })}
-        </Flex>
+        <>
+            <Flex
+                flexDir={"column"}
+                alignItems={"center"}
+            >
+                <Input
+                    placeholder={"Search project..."}
+                />
+            </Flex>
+            <Flex
+                flexDir={"column"}
+                alignItems={"center"}
+                gap={12}
+            >
+                {projectData.map((data, index) => {
+                    return (
+                        <ProjectItem
+                            key={index}
+                            title={data.title}
+                            description={data.description}
+                            srcUrl={data.srcUrl}
+                            demoUrl={data.demoUrl}
+                            tags={data.tags}
+                        />
+                    );
+                })}
+            </Flex>
+        </>
     );
 }
